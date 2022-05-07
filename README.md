f089e4933b3ceb7c51ac7de0eea66828d9f4eed2
LearnPythonWithRune <learnpythonwithrune@gmail.com>
2022-05-07 12:59:11 +0200

    added files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # capstone-infrastructure
 Setting up Infrastructure Example
+
+Build mysql database
+``` bash
+docker build -t capstone-mysql -f Dockerfile.mysql .
+```
+
+## Start them all
+``` bash
+docker compose up
+```
---
+++ schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS `DB`;
+CREATE DATABASE `DB`;
+USE `DB`;
+
+DROP TABLE IF EXISTS `ingress`;
+CREATE TABLE `ingress` (
+  `crawl_time` datetime,
+  `ingest_time` datetime,
+  `pipeline_name` varchar(256) NOT NULL,
+  `ingest_key` varchar(256) NOT NULL,
+  `ingest_value` varchar(1024) NOT NULL
+);
+
+DROP TABLE IF EXISTS `egress`;
+CREATE TABLE `egress` (
+  `report_time` datetime,
+  `city` varchar(256) NOT NULL,
+  `weather` varchar(256) NOT NULL,
+  `temperature` int
+);
+
+DROP TABLE IF EXISTS `error_log`;
+CREATE TABLE `error_log` (
+  `ingest_time` datetime,
+  `error_time` datetime,
+  `pipeline_name` varchar(256) NOT NULL,
+  `ingest_key` varchar(256) NOT NULL,
+  `ingest_value` varchar(256) NOT NULL,
+  `error_message` text NOT NULL
+);
